18c64dc87804cf3b686815bc081056a24f2aca7c
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-09-26 23:08:33 +0300

    Create function for updating gameboard array values
    Moved renderBoard function from the main module to the gameBoard object.

--- js/javascript.js
+++ js/javascript.js
@@ -2,16 +2,35 @@
   // Cache DOM elements
   const gameboardElementArray = document.querySelectorAll(".gameboard div");
 
-  // Create object to store the gameboard array and functions for changing the values inside it
+  // Create object to store the gameboard array and functions for displaying gameboard and changing arrays values
   const gameBoard = (() => {
+
+    // Create gameboard array
     const gameboard = [];
+
     // Function for populating gameboard array with empty values
     function populateArray(location) {
       gameboard.push(location.textContent)
     };
 
+    // Function for displaying gameboard array values on the DOM
+    function renderBoard() {
+      for (i = 0; i < gameboardElementArray.length; i++) {
+        gameboardElementArray[i].textContent = gameBoard.gameboard[i];
+      }
+    };
+
+    // Update array values
+    function updateArray(event, marker) {
+      const dataIndex = event.getAttribute("data-index");
+      if (gameboard[dataIndex] === "") {
+        gameboard[dataIndex] = marker;
+      }
+    };
+
     gameboardElementArray.forEach(item => populateArray(item));
-    return {gameboard};
+
+    return {gameboard, updateArray, renderBoard};
   })();
 
   // Factory function for creating players
@@ -19,13 +38,6 @@
     const getPlayerName = () => name;
     const getPlayerMarker = () => marker;
     return {getPlayerMarker, getPlayerName}
-  };
-
-  // Function for displaying gameboard array values on the DOM
-  function renderBoard() {
-    for (i = 0; i < gameboardElementArray.length; i++) {
-      gameboardElementArray[i].textContent = gameBoard.gameboard[i];
-    }
   };
 
   const playerOne = Player("Mr. Pink", "X");
@@ -49,16 +61,20 @@
     }
     
     function addMark(item) {
+      const currentPlayerMarker = currentPlayer.getPlayerMarker();
+
       if (item.textContent === "") {
-        item.textContent = currentPlayer.getPlayerMarker();
+        item.textContent = currentPlayerMarker;
+        trackCurrentPlayer();
+        gameBoard.updateArray(item, currentPlayerMarker);
+        console.log(gameBoard.gameboard);
       }
-      trackCurrentPlayer();
-      console.log(gameBoard.gameboard);
     }
 
     function startGame() {
-      renderBoard();
-      // Event listener for adding marks to the board when the corresponding html element is clicked
+      // Display empty board for a new game
+      gameBoard.renderBoard();
+      // Create event listener for adding marks to the board when the corresponding html element is clicked
       gameboardElementArray.forEach(item => {
         item.addEventListener("click", () => game.addMark(item));
       });
