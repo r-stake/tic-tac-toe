7554f4349ae711aba6092fb6a570431ad0aedf5c
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-09-26 22:32:35 +0300

    Create "game" module, to oversee game progress
    Created function for switching between both players.
    Created function responsible for placing current players mark on the board,
    when the html element has been clicked.

--- js/javascript.js
+++ js/javascript.js
@@ -2,45 +2,72 @@
   // Cache DOM elements
   const gameboardElementArray = document.querySelectorAll(".gameboard div");
 
+  // Create object to store the gameboard array and functions for changing the values inside it
   const gameBoard = (() => {
-
     const gameboard = [];
+    // Function for populating gameboard array with empty values
+    function populateArray(location) {
+      gameboard.push(location.textContent)
+    };
 
+    gameboardElementArray.forEach(item => populateArray(item));
     return {gameboard};
   })();
 
+  // Factory function for creating players
   const Player = (name, marker) => {
     const getPlayerName = () => name;
     const getPlayerMarker = () => marker;
-    return {getPlayerName, getPlayerMarker};
+    return {getPlayerMarker, getPlayerName}
   };
 
-  const playerOne = Player('playerOne', "X");
-  const playerTwo = Player('playerTwo', "O");
-
-  console.log(playerOne.getPlayerName());
-  console.log(playerTwo.getPlayerName());
-
-  gameBoard.gameboard.push("X");
-  gameBoard.gameboard.push("O");
-  gameBoard.gameboard.push("X");
-  gameBoard.gameboard.push("O");
-  gameBoard.gameboard.push("X");
-  gameBoard.gameboard.push("O");
-  gameBoard.gameboard.push("X");
-  gameBoard.gameboard.push("O");
-  gameBoard.gameboard.push("X");
-
-  console.log(gameBoard.gameboard);
-
+  // Function for displaying gameboard array values on the DOM
   function renderBoard() {
     for (i = 0; i < gameboardElementArray.length; i++) {
       gameboardElementArray[i].textContent = gameBoard.gameboard[i];
     }
   };
 
-  renderBoard();
+  const playerOne = Player("Mr. Pink", "X");
+  const playerTwo = Player("Mr. Yellow", "O");
+  console.log(playerOne);
+  console.log(playerTwo);
+  console.log(playerOne.getPlayerName());
+  console.log(playerOne.mark);
+  console.log(playerTwo.getPlayerName());
 
+  const game = (() => {
+    // let gameWon = false;
+    let currentPlayer = playerOne;
 
+    function trackCurrentPlayer() {
+      if (currentPlayer === playerOne) {
+        currentPlayer = playerTwo;
+      } else {
+        currentPlayer = playerOne;
+      }
+    }
+    
+    function addMark(item) {
+      if (item.textContent === "") {
+        item.textContent = currentPlayer.getPlayerMarker();
+      }
+      trackCurrentPlayer();
+      console.log(gameBoard.gameboard);
+    }
+
+    function startGame() {
+      renderBoard();
+      // Event listener for adding marks to the board when the corresponding html element is clicked
+      gameboardElementArray.forEach(item => {
+        item.addEventListener("click", () => game.addMark(item));
+      });
+    }
+
+    startGame();
+
+    return {addMark}
+
+  })();
 
 })();
